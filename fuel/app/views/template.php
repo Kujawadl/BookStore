1dee097e5fdcb68c2b88dd052935af10fc7b8fdd
Dylan Jager-Kujawa <dylanleekujawa@gmail.com>
2016-11-30 12:55:56 -0600

    Styling updates

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -49,22 +49,7 @@
 	<div class="container">
 		<div class="col-md-12">
 			<h1><?php echo $title; ?></h1>
-<?php if (Session::get_flash('success')): ?>
-			<div class="alert alert-success">
-				<strong>Success</strong>
-				<p>
-				<?php echo implode('</p><p>', e((array) Session::get_flash('success'))); ?>
-				</p>
-			</div>
-<?php endif; ?>
-<?php if (Session::get_flash('error')): ?>
-			<div class="alert alert-danger">
-				<strong>Error</strong>
-				<p>
-				<?php echo implode('</p><p>', e((array) Session::get_flash('error'))); ?>
-				</p>
-			</div>
-<?php endif; ?>
+      <?php echo View::forge('flash'); ?>
 		</div>
 		<div class="col-md-12">
       <?php echo $content; ?>
