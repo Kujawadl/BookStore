e108188efa01f2eb24f2ddcc9c92cad7c8aaf3be
Dylan Jager-Kujawa <dylanleekujawa@gmail.com>
2016-11-30 10:30:56 -0600

    Correct error with contact helper models

--- fuel/app/classes/controller/cart.php
+++ fuel/app/classes/controller/cart.php
@@ -2,7 +2,7 @@
 
 class Controller_Cart extends Controller_Template
 {
-  public $Cart = NULL;
+  public $Cart;
 
   /**
    * Gets the current user's cart, or creates one if none exists.
@@ -14,7 +14,7 @@ class Controller_Cart extends Controller_Template
    */
   public function before()
   {
-    global $Cart;
+    parent::before();
 
     if (!Auth::check())
     {
@@ -29,13 +29,14 @@ class Controller_Cart extends Controller_Template
       if (count($Query) == 0)
       {
         // Create a new cart
-        $Cart = Model_Order::forge();
-        $Cart->Customer = $UserId;
-        $Cart->Save();
+        $this->Cart = Model_Order::forge();
+        $this->Cart->Customer = $UserId;
+        $this->Cart->Save();
       } elseif (count($Query) == 1) {
         // Get the current cart
-        $Cart = $Query[0];
+        $this->Cart = $Query;
       } else {
+        Session::set_flash('error', 'Your cart could not be found...');
         // @TODO Delete all possible carts and notify user there was an error.
       }
     }
@@ -56,9 +57,9 @@ class Controller_Cart extends Controller_Template
    */
   public function action_view()
   {
-    $data['Cart'] = $Cart;
+    $data['Cart'] = $this->Cart;
     $this->template->title   = "View Cart";
-    $this->template->content = View::forge('list/cart', $data);
+    $this->template->content = View::forge('lists/items', $data);
   }
 
   /**
@@ -74,7 +75,8 @@ class Controller_Cart extends Controller_Template
    */
   public function action_add($ItemId, $Quantity = 1)
   {
-    global $Cart;
+    isset($this->Cart) or Response::redirect('/cart/view');
+
     $Book = Model_Book::find($ItemId);
 
     if ($Book == NULL)
@@ -83,12 +85,12 @@ class Controller_Cart extends Controller_Template
     } else {
       $Item = Model_OrderItem::query()
                ->where('book', '=', $Book->id)
-               ->where('order', '=', $Cart->id)
+               ->where('order', '=', $this->Cart->id)
                ->get_one();
       if ($Item == NULL)
       {
         $Item = Model_OrderItem::forge();
-        $Item->Order    = $Cart;
+        $Item->Order    = $this->Cart;
         $Item->Book     = $Book;
         $Item->Quantity = 0;
       }
@@ -112,7 +114,8 @@ class Controller_Cart extends Controller_Template
    */
   public function action_update($ItemId, $Quantity = 1)
   {
-    global $Cart;
+    isset($this->Cart) or Response::redirect('/cart/view');
+
     $Book = Model_Book::find($ItemId);
 
     if ($Book == NULL)
@@ -121,12 +124,12 @@ class Controller_Cart extends Controller_Template
     } else {
       $Item = Model_OrderItem::query()
                ->where('book', '=', $Book->id)
-               ->where('order', '=', $Cart->id)
+               ->where('order', '=', $this->Cart->id)
                ->get_one();
       if ($Item == NULL)
       {
         $Item = Model_OrderItem::forge();
-        $Item->Order = $Cart;
+        $Item->Order = $this->Cart;
         $Item->Book  = $Book;
       }
       $Item->Quantity = $Quantity;
@@ -151,7 +154,8 @@ class Controller_Cart extends Controller_Template
    */
   public function action_remove($ItemId, $Quantity = NULL)
   {
-    global $Cart;
+    isset($this->Cart) or Response::redirect('/cart/view');
+
     $Book = Model_Book::find($ItemId);
 
     if ($Book == NULL)
@@ -160,7 +164,7 @@ class Controller_Cart extends Controller_Template
     } else {
       $Item = Model_OrderItem::query()
                ->where('book', '=', $Book->id)
-               ->where('order', '=', $Cart->id)
+               ->where('order', '=', $this->Cart->id)
                ->get_one();
       if ($Item != NULL)
       {
@@ -187,6 +191,8 @@ class Controller_Cart extends Controller_Template
    */
   public function action_place()
   {
+    isset($this->Cart) or Response::redirect('/cart/view');
+
     // @TODO: Ask customer for shipping address
 
     // Using a SQL query, we can use the MySQL NOW() function, which ensures
