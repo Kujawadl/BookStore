2f980b67fd23fb8b9a1eaeb396c9d9a6e728061a
Dylan Jager-Kujawa <dylanleekujawa@gmail.com>
2016-11-30 12:35:53 -0600

    Enable remove from cart

--- public/assets/js/site.js
+++ public/assets/js/site.js
@@ -12,4 +12,23 @@ $(document).ready(function() {
     qty = $(this).children('input').val();
     $(this).attr('action', '/cart/update/' + id + '/' + qty);
   });
+
+  $(".btnRemoveFromCart").click(function() {
+    id = $(this).closest(".frmUpdateCart").attr('item');
+    title = $(this).closest('tr').find('.bookTitle').text();
+
+    sweetalert({
+      title: "Are you sure?",
+      text: "Remove " + title + " from cart?",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Yes, remove it!",
+      closeOnConfirm: true
+    },
+    function(isConfirm) {
+      if (isConfirm) {
+        window.location = '/cart/remove/' + id;
+      }
+    });
 });
