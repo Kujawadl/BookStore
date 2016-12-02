98d4d9460f40a6bfcbb99553977313ab24e68f58
Dylan Jager-Kujawa <dylanleekujawa@gmail.com>
2016-12-02 13:06:29 -0600

    Add codesnippet view for debugging
    Uses highlighter.js to render all pre and code elements with class hljs as highlighted code, using github gist format

--- public/assets/js/site.js
+++ public/assets/js/site.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+  // Enable navbar search
   $("#frmSearch").submit(function() {
     if (! $("#txtSearch").val() == "") {
       $("#frmSearch").attr("action", "/browse/search/" + $("#txtSearch").val());
@@ -7,12 +9,14 @@ $(document).ready(function() {
     }
   });
 
+  // Enable update quantity button
   $(".frmUpdateCart").submit(function() {
     id = $(this).attr('item');
     qty = $(this).children('input').val();
     $(this).attr('action', '/cart/update/' + id + '/' + qty);
   });
 
+  // Enable remove from cart button, with sweetalert to confirm action
   $(".btnRemoveFromCart").click(function() {
     id = $(this).closest(".frmUpdateCart").attr('item');
     title = $(this).closest('tr').find('.bookTitle').text();
@@ -33,5 +37,32 @@ $(document).ready(function() {
         }
       }
     );
+  });
+
+  // Enable syntax highlighting on all pre and code elements with class highlight
+  $('pre.hljs, code.hljs').each(function(i, block) {
+    // Create a worker thread for each element
+    var worker = new Worker(
+      URL.createObjectURL(
+        new Blob(
+          [
+            "onmessage = function(event) { \
+              importScripts('/public/assets/js/highlighter.js'); \
+              var result = self.hljs.highlightAuto(event.data); \
+              postMessage(result.value); \
+            }"
+          ],
+          {
+            "type": "text\/plain"
+          }
+        )
+      )
+    );
+    // When the worker returns, update the element's html content
+    worker.onmessage = function(event) {
+      $(block).html(event.data);
+    }
+    // Start the worker
+    worker.postMessage($(block).text());
   });
 });
