699d1f5d632622bed35e6ff576213b4bcc27fbb2
Dylan Jager-Kujawa <dylanleekujawa@gmail.com>
2016-12-04 14:55:54 -0600

    Add 404 and 501 views

---
+++ fuel/app/views/error/404.php
@@ -0,0 +1,4 @@
+<div class="jumbotron">
+  <h1>Error 404: <small>Not Found</small></h1>
+  <p>There's nothing here...</p>
+</div>
---
+++ fuel/app/views/error/501.php
@@ -0,0 +1,4 @@
+<div class="jumbotron">
+  <h1>Error 501: <small>There\'s nothing here...</small></h1>
+  <p>Sorry, but it looks like we haven't gotten around to implementing this part of the site just yet. Check back at a later time.</p>
+</div>
