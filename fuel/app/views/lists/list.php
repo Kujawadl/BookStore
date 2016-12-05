942a7a1ec80f8312964792a0d1d11e1f10cdfc6d
Dylan Jager-Kujawa <dylanleekujawa@gmail.com>
2016-12-04 18:02:08 -0600

    Create separate list view with pagination

---
+++ fuel/app/views/lists/list.php
@@ -0,0 +1,30 @@
+<?php if (isset($Rows) && count($Rows) > 0): ?>
+  <?php
+    $Pages = count($Rows) / 10;
+    $Page  = (isset($_GET['p']) && is_numeric($_GET['p']) ? $_GET['p'] : 1);
+  ?>
+  <table class="table table-condensed table-hover">
+  	<tbody>
+      <?php
+        $Start = 10 * ($Page - 1);
+        $End = $Start + 10;
+        for ($i = $Start; $i < $End; $i++)
+        {
+          echo $Rows[$i];
+        }
+      ?>
+    </tbody>
+    <?php
+      if ($Pages > 1)
+      {
+        $data['BaseUrl'] = $BaseUrl;
+        $data['Pages']   = $Pages;
+        $data['Page']    = $Page;
+
+        echo View::forge('lists/pagination', $data);
+      }
+    ?>
+  </table>
+<?php else: ?>
+  <p>No results.</p>
+<?php endif; ?>
